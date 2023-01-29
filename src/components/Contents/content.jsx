2e88eff478ce511ba9bca60c8cba08c164b242bc
johnbrian <johnbriancapate@gmail.com>
2023-01-30 02:48:15 +0800

    add paragraph and Icons

--- src/components/Contents/content.jsx
+++ src/components/Contents/content.jsx
@@ -1,9 +1,10 @@
 import react from 'react';
 import '../Contents/content.css';
+import Icons from "../Icons/icons"
 
 function Content() {
   return (
-    <div>
+    <div className="contentWrapper">
       <h3 className="paragraphTitle">About me</h3>
       <div className="paragraphContent">
         <p>
@@ -25,6 +26,27 @@ function Content() {
           be.
         </p>
       </div>
+      <h3 className="paragraphTitle">Skills</h3>
+      <div className="paragraphContent">
+        <p>
+          When I was a senior high-school student my programming language was
+          Java, HTML, CSS, and JavaScript.
+        </p>
+        <p>
+          After learning C#, Java, and python at school. JavaScript is the
+          language that I liked the most.
+        </p>
+        <p>
+          I’m interested in other languages and frameworks out there like
+          typescript, Laravel and many more.
+        </p>
+        <p>
+          I’m willing to learn more and enhance my skills to become a better
+          programmer here is some of them and the tools that currently use:
+        </p>
+        <Icons/>
+      </div>
+      
     </div>
   );
 }
