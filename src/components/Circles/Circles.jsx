7fd50c91c45caa01a3d3799ff5e78d20d19586ee
johnbrian <johnbriancapate@gmail.com>
2023-02-02 23:32:01 +0800

    last file bago pumunta sa bahay nila ate at brother inlaw

--- src/components/Circles/Circles.jsx
+++ src/components/Circles/Circles.jsx
@@ -1,10 +1,15 @@
 import react from 'react';
-import "../Circles/Circles.css";
-
+import '../Circles/Circles.css';
+/* okay ganito SVG iedit mo isa isa ung mga circles */
 function Circles() {
   return (
-    <div className='circles'>
-      /* okay ganito SVG iedit mo isa isa ung mga circles */
+    <div className="circles">
+      <div className="circleOne">
+        
+      </div>
+      <div className="circleTwo">
+        
+      </div>
     </div>
   );
 }
